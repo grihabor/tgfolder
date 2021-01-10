e4f56bcb011ab289dc9bbf5d6287bb5d23272469
Borodin Gregory <grihabor@gmail.com>
2021-01-11 02:03:25 +0300

    Implement `tgfolder include_peers add`

--- src/tgfolder/lib.py
+++ src/tgfolder/lib.py
@@ -1,28 +1,160 @@
+import asyncio
 import json
 import os
 import pathlib
-from typing import Optional, List
+from typing import Optional, List, Any, Dict, Set
 
 import click
 from pydantic.dataclasses import dataclass
 from telethon import TelegramClient
 from telethon.sessions import SQLiteSession
 from telethon.tl import types
-from telethon.tl.functions import messages
+from telethon.tl.functions import messages, contacts
 
 
-async def async_command_user_list(config: Optional[str], target_title: str):
+async def async_command_include_peers_group_add(
+    config: Optional[str], folder_title: str, chat_ids: List[int], dry_run: bool
+):
     client = await new_client(config)
+    dialog_filter = await get_dialog_filter(client, folder_title)
+    if dialog_filter is None:
+        raise RuntimeError(f"Folder not found: {folder_title}")
+
+    chats_we_want = set(chat_ids)
+    chats_already_included = set(
+        get_entity_ids(dialog_filter.include_peers)
+        + get_entity_ids(dialog_filter.pinned_peers)
+    )
+    chats_to_add = chats_we_want - chats_already_included
+
+    await client.get_dialogs(ignore_pinned=False)
+    entities = await asyncio.gather(
+        *[client.get_entity(chat_id) for chat_id in chats_to_add]
+    )
+
+    chats = filter_chats(entities)
+    channels = filter_channels(entities)
+
+    assert len(channels) + len(chats) == len(
+        entities
+    ), f"{len(channels)} + {len(chats)} != {len(entities)}"
+
+    echo_json(chats_to_list(chats))
+    echo_json(channels_to_list(channels))
+    if dry_run:
+        return
+
+    peer_chats = [types.InputPeerChat(c.id) for c in chats]
+    peer_channels = [types.InputPeerChannel(c.id, c.access_hash) for c in channels]
+    dialog_filter.include_peers = (
+        dialog_filter.include_peers + peer_chats + peer_channels
+    )
+    request = messages.UpdateDialogFilterRequest(dialog_filter.id, dialog_filter)
+    result = await client(request)
+    click.echo(
+        f'Added {len(entities)} entries to folder "{folder_title}": {result}', err=True
+    )
+
+
+def filter_chats(entities):
+    return [e for e in entities if isinstance(e, types.Chat)]
+
+
+def filter_channels(entities):
+    return [e for e in entities if isinstance(e, types.Channel)]
+
+
+def filter_users(entities):
+    return [e for e in entities if isinstance(e, types.User)]
+
+
+def chats_to_list(chats: List[types.Chat]) -> List[Dict[str, Any]]:
+    return [{"type": "chat", "id": e.id, "title": e.title} for e in chats]
+
+
+def channels_to_list(channels: List[types.Channel]) -> List[Dict[str, Any]]:
+    return [{"type": "channel", "id": e.id, "title": e.title} for e in channels]
+
+
+def users_to_list(users: List[types.User]) -> List[Dict[str, Any]]:
+    return [{"type": "user", "id": e.id, "username": e.username} for e in users]
+
+
+def get_entity_ids(peers: List[types.TypeInputPeer]) -> List[int]:
+    entity_ids = [get_entity_id(peer) for peer in peers]
+    result = [entity_id for entity_id in entity_ids if entity_id is not None]
+    assert len(peers) == len(result), f"{len(peers)} != {len(result)}"
+    return result
+
+
+def get_entity_id(e: types.TypeInputPeer) -> Optional[int]:
+    if isinstance(e, types.InputPeerChat):
+        return e.chat_id
+    if isinstance(e, (types.InputPeerUser, types.InputPeerUserFromMessage)):
+        return e.user_id
+    if isinstance(e, (types.InputPeerChannel, types.InputPeerChannelFromMessage)):
+        return e.channel_id
+    return None
+
+
+async def async_command_common_chat_list(config: Optional[str], user_ids: List[int]):
+    client = await new_client(config)
+
+    async def get_common_chats(user_id: int) -> Set[int]:
+        result = await client(
+            messages.GetCommonChatsRequest(user_id, max_id=2147483647, limit=100)
+        )
+        chats: List[types.Chat] = result.chats
+        return set(chat.id for chat in chats)
+
+    chat_batches: List[Set[int]] = await asyncio.gather(
+        *[get_common_chats(user_id) for user_id in user_ids]
+    )
+    chat_ids = set()
+    for batch in chat_batches:
+        chat_ids.update(batch)
+
+    entities = await asyncio.gather(*[client.get_entity(peer) for peer in chat_ids])
+    chats = filter_chats(entities)
+    channels = filter_channels(entities)
+    users = filter_users(entities)
+
+    echo_json(chats_to_list(chats))
+    echo_json(channels_to_list(channels))
+    echo_json(users_to_list(users))
+
+
+async def get_dialog_filter(client, title: str) -> Optional[types.DialogFilter]:
     dialog_filters: List[types.DialogFilter] = await client(
         messages.GetDialogFiltersRequest()
     )
-    found = [f for f in dialog_filters if f.title == target_title]
+    found = [f for f in dialog_filters if f.title == title]
     if not found:
-        click.echo(f"folder not found: {target_title}", err=True)
         return
 
-    target = found[0]
-    echo_json([p.to_dict() for p in target.include_peers])
+    return found[0]
+
+
+async def async_command_user_list(config: Optional[str], folder_title: str):
+    client = await new_client(config)
+    dialog_filter = await get_dialog_filter(client, folder_title)
+    if dialog_filter is None:
+        click.echo(f"folder not found: {folder_title}", err=True)
+        return
+
+    entities = await asyncio.gather(
+        *[
+            client.get_entity(peer)
+            for peer in dialog_filter.include_peers + dialog_filter.pinned_peers
+        ]
+    )
+    chats = filter_chats(entities)
+    channels = filter_channels(entities)
+    users = filter_users(entities)
+
+    echo_json(chats_to_list(chats))
+    echo_json(channels_to_list(channels))
+    echo_json(users_to_list(users))
 
 
 async def new_client(config: Optional[str]):
@@ -45,8 +177,10 @@ async def async_command_list(config: Optional[str]):
     echo_json(titles)
 
 
-def echo_json(obj):
-    click.echo(json.dumps(obj, ensure_ascii=False))
+def echo_json(arr: List[Any]):
+    if len(arr) == 0:
+        return
+    click.echo("\n".join(json.dumps(s, ensure_ascii=False) for s in arr))
 
 
 async def new_session(path: str) -> SQLiteSession:
--- src/tgfolder/main.py
+++ src/tgfolder/main.py
@@ -1,8 +1,13 @@
 import asyncio
-from typing import Optional
+from typing import Optional, List
 
 import click
-from tgfolder.lib import async_command_user_list, async_command_list
+from tgfolder.lib import (
+    async_command_user_list,
+    async_command_list,
+    async_command_common_chat_list,
+    async_command_include_peers_group_add,
+)
 
 config_path_option = click.option("-c", "--config", default=None)
 
@@ -23,11 +28,33 @@ def include_peers_group():
     pass
 
 
+@include_peers_group.command(name="add")
+@config_path_option
+@click.option("-t", "--title", required=True)
+@click.option("--dry-run/--exec", default=True)
+@click.argument("chat_ids", nargs=-1, type=click.INT)
+def command_include_peers_group_add(
+    config: Optional[str], title: str, chat_ids: List[int], dry_run: bool
+):
+    asyncio.run(
+        async_command_include_peers_group_add(
+            config, folder_title=title, chat_ids=chat_ids, dry_run=dry_run
+        )
+    )
+
+
+@main_group.command(name="common_chat_list")
+@config_path_option
+@click.argument("user_ids", nargs=-1, type=click.INT)
+def command_common_chats(config: Optional[str], user_ids: List[int]):
+    asyncio.run(async_command_common_chat_list(config, user_ids))
+
+
 @include_peers_group.command(name="list")
 @config_path_option
 @click.option("-t", "--title", required=True)
 def command_user_list(config: Optional[str], title):
-    asyncio.run(async_command_user_list(config, target_title=title))
+    asyncio.run(async_command_user_list(config, folder_title=title))
 
 
 def main():
